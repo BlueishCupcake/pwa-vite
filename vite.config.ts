a790ac3f35c962215c055cb7b37c72283def3e0f
sophiequines <sophiequines@gmail.com>
2024-02-26 15:35:05 -0300

    feat: add pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,43 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['react.svg'],
+      manifest: {
+        name: 'Vite PWA',
+        short_name: 'Vite PWA',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: 'vite.svg',
+            sizes: '64x64',
+            type: 'image/png',
+          },
+          {
+            src: 'vite.svg',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'vite.svg',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: 'viteMaskable.png',
+            sizes: '196x196',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+    }),
+  ],
+});
